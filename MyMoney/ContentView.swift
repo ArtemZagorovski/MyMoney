6cc6c5dc9d42464978164262e3ddc6658a309a6f
artemzagorovski@mail.ru <Artem Zagorovsky>
2021-01-14 14:05:44 +0300

    MMIOS-12: SwiftUI project created

---
+++ MyMoney/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMoney
+//
+//  Created by Zagorovsky, Artem on 1/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
